d3573c7979194400f370ff2c78fc58af8434ea29
Anand Patel <anand.patel@neofinancial.com>
2022-07-22 22:54:33 -0600

    scaffold project

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { SWRConfig } from "swr";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <SWRConfig value={{ refreshInterval: 30000, revalidateOnFocus: false }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
